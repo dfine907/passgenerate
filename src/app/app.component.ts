2881c0dadd6d2f11cae41ef437e0b294ad231aa0
Dina <dinagiacobbi@gmail.com>
2024-02-07 14:38:25 -0900

    Adding button and event listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'passgenerate';
+  password = ''
+  onButtonClick(){
+    this.password = 'my password';
+    
+  }
 }
